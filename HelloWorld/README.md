68cc443c05d1f387d358103cb4cafd20d037afbd
SomeLeftGuy <59534651+SomeLeftGuy@users.noreply.github.com>
2023-10-19 09:27:37 +0200

    Upload web-counter app

---
+++ HelloWorld/README.md
@@ -0,0 +1,15 @@
+# README #
+
+* A Flask web app running in docker using redis to count the view and print hostname of container
+
+* To build and run the server after cloning the repo use:
+```
+docker-compose up --build -d
+        or
+docker-compose up --build
+```
+
+* dockerhub image
+```
+https://hub.docker.com/r/thestrider/helloworld/
+```
---
+++ HelloWorld/app.py
@@ -0,0 +1,23 @@
+from flask import Flask, render_template
+from redis import Redis
+import socket
+
+app = Flask(__name__)
+redis = Redis(host='localhost', port=6379)
+
+
+@app.route("/")
+def home():
+    redis.incr('hits')
+    gethits = redis.get('hits')
+    hits = gethits.decode('utf-8')
+    hostname = socket.gethostname()
+    return render_template("index.html", hits=hits, hostname=hostname)
+
+
+# if __name__ == "__main__":
+#     app.run(host='0.0.0.0', debug=True, port=8080)
+
+# docker
+if __name__ == "__main__":
+    app.run(debug=True)
